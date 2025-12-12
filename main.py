999d928adeaac477874f453348dcfff1534b0333
VitonXL <Saha300351@mail.ru>
2025-12-12 09:43:23 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-
-# Теперь это — веб-сервер
-from web.app import app
-
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
