2cba8ce51781e7e1f0b5ee3978f7f6beecab8101
VitonXL <Saha300351@mail.ru>
2025-12-13 17:43:33 +0300

    Update main.py

--- web/main.py
+++ web/main.py
@@ -1,14 +1,15 @@
 # web/main.py
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from .routes import router
 
 app = FastAPI(title="Лео Помощник — UI")
 
-# Статические файлы
+# Подключаем статику: /static/style.css
 app.mount("/static", StaticFiles(directory="web/static"), name="static")
 
-# Маршруты
+# Подключаем маршруты
 app.include_router(router)
 
 @app.get("/health")
