792613a791f43125d6816623b37777ea306b4eaf
VitonXL <Saha300351@mail.ru>
2025-12-13 15:45:57 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,10 @@
+# bot/config.py
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")  # Будет от Railway
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")   # Если используешь webhook
+PORT = int(os.getenv("PORT", 8000))
